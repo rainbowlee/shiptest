3394713373b1baa1ce3323c1b670da2a45a7c550
lihongyu <283576530@qq.com>
2021-06-03 16:12:24 +0800

    zengjia

--- BagTest.h
+++ BagTest.h
@@ -1,12 +1,18 @@
 #include <gtest/gtest.h>
-//#include "gamed/UserMgr/UItemBagMgr.h"
-//#include "gamed/logic/User.h"
-//#include "gamed/logic/Player.h"
-//#include "gamed/GameDataHandler.h"
-//#include "event/DceItemBag.pb.h"
-//#include "public/Config/tblCode/ItemTbl.h"
-//#include "public/Config/tblCode/GiftDropTbl.h"
-//#include "public/common/Clock.h"
+#include <iostream>
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "../logic/User.h"
+#include "../logic/Player.h"
+#include "../logic/BattleMgr.h"
+#include "../logic/WishGiftMgr.h"
+#include "../logic/StageMgr.h"
+#include "../logic/dbinterface.pb.h"
+#include "../logic/StageTbl.h"
+#include "../common/const_def.h"
+#include "../common/type_def.h"
+
 class BagTest : public testing::Test
 {
 protected:
@@ -61,15 +67,110 @@ TEST_F(BagTest, ItemTest)
 }
 
 
+TEST(BattleTest2, NormalTest)
+{
+}
+
 TEST(BattleTest, NormalTest)
 {
-	const char* const str = "world";
+	
+	//for( int i = 0; i < 100; i++)
+	{
+		User* pUser = new User;
+		pUser->Init();
+		pUser->InitNewUser();
+		Player* pPlayer = pUser->GetPlayer();
+		pPlayer->SetLevel(1000);
 
-	const char* p = str;
-	//EXPECT_FALSE(SkipPrefix("W", &p));
-	EXPECT_EQ(str, p);
+		WishGiftMgr* mgr = pPlayer->GetWishGiftMgr();
+		mgr->SetNewDay();
+		//mgr->TriggerTask();
+		const char* const str = "world";
 
-	p = str;
-	//EXPECT_FALSE(SkipPrefix("world!", &p));
-	EXPECT_EQ(str, p);
+		const char* p = str;
+		//EXPECT_FALSE(SkipPrefix("W", &p));
+		EXPECT_EQ(str, p);
+
+		p = str;
+		//EXPECT_FALSE(SkipPrefix("world!", &p));
+		EXPECT_EQ(str, p);
+		pPlayer->AddCash(5000000,GM_AddMoney_Add);
+
+		pPlayer->AddToken(TOKEN_TYPE_WishGift, 500001);
+
+		mgr->SetWishGiftId(2);
+		DceWishGift dceproto;
+		dceproto.add_boxindex(0);
+		dceproto.add_boxindex(1);
+		dceproto.add_boxindex(2);
+		dceproto.add_boxindex(3);
+		dceproto.add_boxindex(4);
+		dceproto.set_id(2);
+
+		DseWishGift dseproto;
+
+		//58	0	ÐÄÔ¸±Ò	1	|1|2|3|4|5	|10|25|30|25|10
+		//2135	180	ÈÙÓþ¼ÇÂ¼	2	|4|8|12|16|20	|10|25|30|25|10
+
+		map<string,int> keyCount;
+		for(int i = 0; i < 10000; i++)
+		{
+			dceproto.clear_boxindex();
+			dceproto.add_boxindex(i%10);
+			dseproto.mutable_droplist()->clear_droplist();
+			int ret = mgr->OpenBox(&dceproto,&dseproto);
+
+			if(ret != 0)
+			{
+				int test = 0;
+				test++;
+			}
+
+			if( dseproto.mutable_droplist()->droplist_size() == 0)
+			{
+				int test1 = 0;
+				test1++;
+			}
+
+
+			int id = dseproto.mutable_droplist()->droplist(0).id();
+			int count = dseproto.mutable_droplist()->droplist(0).count();
+
+
+			string key = toString(id) + "_" + toString(count);
+
+			auto iter = keyCount.find(key);
+			if(iter == keyCount.end())
+			{
+				keyCount.insert(make_pair(key,0));
+				iter = keyCount.find(key);
+			}
+
+			iter->second++;
+		}
+
+
+		for(int r = 0; r < 100; r++)
+		{
+			dseproto.Clear();
+			mgr->ShopRefresh(&dseproto);
+			
+			int buycount = 0;
+			for(int buyi = 0; buyi < dseproto.shoplist_size(); buyi++)
+			{
+				if(buyi%2==1)
+				{
+					int id = dseproto.shoplist(buyi).id();
+					DseWishGift dseprotoret;
+					mgr->ShopBuy(id,&dseprotoret);
+				}
+			}
+
+		}
+		
+		dseproto.Clear();
+
+
+		mgr->ShopRefresh(&dseproto);
+	}
 }
--- Test.cpp
+++ Test.cpp
@@ -1,11 +1,12 @@
 //#include "ClientTest.h"
-#include "BagTest.h"
+//#include "BagTest.h"
 //#include "taskTest.h"
 //#include "RedisTest.h"
 //#include "LotteryTest.h"
 //#include "CardTest.h"
 //#include "PVPMatchTest.h"
 //#include "pveTest.h"
+#include "VipQuickSweepTest.h"
 int PullInMyLibrary() 
 {
 	return 0;
